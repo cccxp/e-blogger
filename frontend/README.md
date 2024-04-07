b114db66eeb020c01db711f6c1c3ff678f981d9f
cccxp <cccxp1@outlook.com>
2024-04-08 01:53:54 +0800

    - add static folder. - create frontend repository folder.

---
+++ frontend/README.md
@@ -0,0 +1,6 @@
+# Frontend project repository
+
+Put frontend project source codes here. 
+
+Copy compiled files into `/static` folder so that fastapi can handle it properly.
+
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from fastapi.staticfiles import StaticFiles
 from routers import account
 
+
 app = FastAPI()
-app.include_router(account.router)
+
+api = APIRouter(prefix='/api/v1')
+api.include_router(account.router)
+
+app.include_router(api)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/", description="used to verify server started successfully.")
 def demo():
