72380c92888581da89364476dafb4fb8b4b8575f
cccxp <cccxp1@outlook.com>
2024-04-14 03:16:53 +0800

    Implement user login functionality.

--- routers/account.py
+++ routers/account.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+import sqlalchemy
 from api_models.account import *
 from database_models.account import User
 
@@ -7,13 +8,31 @@ from database import AsyncSession
 
 router = APIRouter(prefix='/account')
 
-class AddUser:
+class UseCaseBase:
     def __init__(self, session: AsyncSession) -> None:
         self.async_session = session
 
-    async def execute(self, email: str, username: str, hashed_password: str) -> None:
+
+class AddUser(UseCaseBase):
+    async def execute(self, email: str, username: str, hashed_password: str) -> tuple[bool, str]:
         async with self.async_session.begin() as session:
-            user = await User.add(session, email, username, hashed_password)
+            try:
+                user = await User.add(session, email, username, hashed_password)
+                return True, 'register success.'
+            except sqlalchemy.exc.IntegrityError:
+                return False, 'email or username already exist.'
+
+
+class LoginCheck(UseCaseBase):
+    async def execute(self, email: str, password: str) -> tuple[bool, str]:
+        async with self.async_session.begin() as session:
+            user = await User.get_by_email(session, email)
+            print(user, email, password)
+            if not user:
+                return False, 'User not found. '
+            if not security.password_validation(password, user.password):
+                return False, 'Password wrong. '
+            return True, 'Login success.'
 
 
 @router.post('/register', response_model=RegisterResponse)
@@ -25,10 +44,14 @@ async def register(r: RegisterRequest, use_case: AddUser = Depends(AddUser)):
         return RegisterResponse(success=False, message=f'Password strength check faild, reason: {reason}')
     # TODO: captcha check
 
-    await use_case.execute(r.email, r.username, security.password_encryption(r.password))
-    return RegisterResponse(success=True, message='register sucess.')
+    success, reason = await use_case.execute(r.email, r.username, security.password_encryption(r.password))
+    return RegisterResponse(success=success, message=reason)
 
 
 @router.post('/login', response_model=LoginResponse)
-async def login(r: LoginRequest):
-    pass 
+async def login(r: LoginRequest, use_case: LoginCheck = Depends(LoginCheck)):
+    token = ''
+    success, reason = await use_case.execute(r.email, r.password)
+    if success:
+        token = security.jwt_encode(r.email)
+    return LoginResponse(success=success, message=reason, token=token)
--- utils/security.py
+++ utils/security.py
@@ -1,6 +1,11 @@
 
 import re 
 import bcrypt
+import jwt 
+import datetime
+import os
+import dotenv
+
 
 def email_check(email: str) -> bool:
     email_regex = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b')
@@ -17,6 +22,7 @@ def password_strength_check(password: str) -> tuple[bool, str]:
         return False, 'password must contain number and alphabet.'
     return True, 'password is strong.'
 
+
 def password_encryption(password: str):
     salt = bcrypt.gensalt(rounds=12)
     hashed_password = bcrypt.hashpw(password.encode(), salt)
@@ -25,3 +31,23 @@ def password_encryption(password: str):
 
 def password_validation(password: str, hashed_password: str):
     return bcrypt.checkpw(password.encode(), hashed_password.encode())
+
+
+def jwt_encode(email: str) -> str:
+    payload = {
+        'exp': datetime.datetime.now() + datetime.timedelta(days=14),
+        'iat': datetime.datetime.now(),
+        'iss': 'e-blogger',
+        'data': {
+            'email': email
+        }
+    }
+    dotenv.load_dotenv() 
+    token = jwt.encode(payload, key=os.environ.get('JWT_SECRET_KEY', 'DEFAULT_SECRET_KEY'), algorithm='HS256')
+    return token 
+
+
+def get_current_user(token: str):
+    # TODO
+    pass 
+
