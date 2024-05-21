d044fbde37903da8483a7a3d6bae8c6bef7e21d5
cccxp <cccxp1@outlook.com>
2024-05-22 00:24:36 +0800

    add comment api model

---
+++ api_models/comments.py
@@ -0,0 +1,35 @@
+from pydantic import Field
+from .base import BaseModel, BaseRequestModel, BaseResponseModel
+
+
+class Comment(BaseModel):
+    id: int = Field(None, description="Comment ID")
+    blog_id: int = Field(None, description="Blog ID")
+    content: str = Field(None, description="Comment content")
+    created_at: str = Field(None, description="Comment creation date")
+    user_email: str = Field(None, description="User email")
+
+
+class GetCommentsRequest(BaseRequestModel):
+    blog_id: int = Field(None, description="Blog ID")
+
+
+class GetCommentsResponse(BaseResponseModel):
+    comments: list[Comment] = Field(None, description="List of comments")
+
+
+class CreateCommentRequest(BaseRequestModel):
+    blog_id: int = Field(None, description="Blog ID")
+    content: str = Field(None, description="Comment content")
+
+
+class CreateCommentResponse(BaseResponseModel):
+    comment_id: int = Field(None, description="Comment ID")
+
+
+class DeleteCommentRequest(BaseRequestModel):
+    comment_id: int = Field(None, description="Comment ID")
+
+
+class DeleteCommentResponse(BaseResponseModel):
+    pass
