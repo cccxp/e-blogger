9ae4e5fb6860d6e7087c065f324638c86fc22afb
cccxp <cccxp1@outlook.com>
2024-06-06 23:04:06 +0800

    cooment use case

--- routers/comments.py
+++ routers/comments.py
@@ -9,6 +9,24 @@ from utils.security import get_current_user
 
 router = APIRouter(prefix='/comments')
 
+from database import AsyncSession
+from fastapi.exceptions import HTTPException
+
+
+class UseCaseBase:
+    def __init__(self, session: AsyncSession) -> None:
+        self.async_session = session
+
+
+class AddCommentUseCase(UseCaseBase):
+    async def execute(self, blog_id: int, comment_text: str, user_email: str):
+        pass
+
+
+class DeleteCommentUseCase(UseCaseBase):
+    async def execute(self, blog_id: int, comment_id: int, user_email: str):
+        pass
+
 
 @router.post('/{blog_id}', response_model=CreateCommentResponse)
 async def add_comment(blog_id: int, r: CreateCommentRequest, user_email: str = Depends(get_current_user)):
