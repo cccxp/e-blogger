e8336ffee30d720b2c50293bf11ef174b813c773
cccxp <cccxp1@outlook.com>
2024-06-07 02:55:17 +0800

    fix baseurl bug

--- static/scripts/api.js
+++ static/scripts/api.js
@@ -1,5 +1,6 @@
 const api = axios.create({
     // baseURL: "http://localhost:8000/api/v1"  // for debug use
+    baseURL: "/api/v1"  // for production use
 })
 
 api.interceptors.response.use((resp) => {
